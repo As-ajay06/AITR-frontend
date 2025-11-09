e17cc484c4f08930049a44bd676dee3cbad2327b
As-ajay06 <as2673447@gmail.com>
2025-11-10 02:19:39 +0530

    congif file

---
+++ config/config.js
@@ -0,0 +1,6 @@
+
+export const API_DEPARTMENT_FILE_UPLOAD ='/api/v1/uplaod/deparment'
+export const API_FACULTY_FILE_UPLOAD ='/api/v1/uplaod/faculty'
+export const API_INSTITUTE_FILE_UPLOAD ='/api/v1/uplaod/institute'
+export const API_STUDENT_FILE_UPLOAD ='/api/v1/uplaod/student'
+
