a0c276ce3fad02df2dd18387467c158d54224efd
As-ajay06 <as2673447@gmail.com>
2025-12-07 00:33:05 +0530

    added faculty awards

--- src/AddDataForms/facultyData/AwardsData.jsx
+++ src/AddDataForms/facultyData/AwardsData.jsx
@@ -33,10 +33,10 @@ const AddAwards = () => {
   const onSubmit = async (data, e) => {
     e.preventDefault();
 
+
     const formData = new FormData();
-    const fileInput = document.querySelector("input[type='file']");
-    if (fileInput?.files[0]) {
-      formData.append("file", fileInput.files[0]);
+    if (data.file && data.file[0]) {
+      formData.append("file", data.file[0]);
     }
     try {
       // post : file
